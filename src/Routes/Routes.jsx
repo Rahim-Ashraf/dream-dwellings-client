9bdff003071764d67894ce2a040430518583a6f2
Abdur Rahim <rahim11500000@gmail.com>
2024-05-31 22:01:37 +0600

    Setup Dream Dwellings client

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../pages/Root/Root";
+import Home from "../pages/Home/Home";
+import AllProperties from "../pages/AllProperties/AllProperties";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/all-properties",
+                element: <AllProperties></AllProperties>
+            }
+        ]
+    },
+]);
+
+export default router
